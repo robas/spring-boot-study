3c53c96b9a839b4566bbcaf296d871d80b5bd9a2
Rodrigo Lopes <robas@chocolatefactory.local>
2019-05-27 22:07:02 -0300

    Adding List attribute to Service Entity

--- src/main/java/br/com/rede/sg/resource/Service.java
+++ src/main/java/br/com/rede/sg/resource/Service.java
@@ -1,17 +1,30 @@
 package br.com.rede.sg.resource;
 
+import java.util.List;
+
 public class Service {
 	private long id;
 	private String name;
 	private String repo;
 	private String desc;
+	private List<String> deps;
+	
 
-	public Service(long id, String name, String repo, String desc) {
+	public Service(long id, String name, String repo, String desc, List<String> deps) {
 		super();
 		this.id = id;
 		this.name = name;
 		this.repo = repo;
 		this.desc = desc;
+		this.deps = deps;
+	}
+
+	public List<String> getDeps() {
+		return deps;
+	}
+
+	public void setDeps(List<String> deps) {
+		this.deps = deps;
 	}
 
 	public long getId() {
--- src/main/java/br/com/rede/sg/service/ServiceService.java
+++ src/main/java/br/com/rede/sg/service/ServiceService.java
@@ -11,12 +11,21 @@ public class ServiceService {
 	private static List<br.com.rede.sg.resource.Service> services = new ArrayList<br.com.rede.sg.resource.Service>();
 
 	static {
+		List<String> dep1 = new ArrayList<String>();
+		List<String> dep2 = new ArrayList<String>();
+		List<String> dep3 = new ArrayList<String>();
+		dep1.add("rota 1");
+		dep1.add("rota 2");
 		services.add(
-				new br.com.rede.sg.resource.Service(++serviceCount, "service-A", "repoA", "descrição do service A"));
+				new br.com.rede.sg.resource.Service(++serviceCount, "service-A", "repoA", "descrição do service A", dep1));
+		dep2.add("rota 2");
+		dep2.add("rota 3");
 		services.add(
-				new br.com.rede.sg.resource.Service(++serviceCount, "service-B", "repoB", "descrição do service B"));
+				new br.com.rede.sg.resource.Service(++serviceCount, "service-B", "repoB", "descrição do service B", dep2));
+		dep3.add("rota 3");
+		dep3.add("rota 4");
 		services.add(
-				new br.com.rede.sg.resource.Service(++serviceCount, "service-C", "repoC", "descrição do service C"));
+				new br.com.rede.sg.resource.Service(++serviceCount, "service-C", "repoC", "descrição do service C", dep3));
 	}
 
 	public List<br.com.rede.sg.resource.Service> getServices() {
@@ -36,7 +45,7 @@ public class ServiceService {
 
 	public br.com.rede.sg.resource.Service addService(br.com.rede.sg.resource.Service service) {
 		br.com.rede.sg.resource.Service createdService = new br.com.rede.sg.resource.Service(++serviceCount,
-				service.getName(), service.getRepo(), service.getDesc());
+				service.getName(), service.getRepo(), service.getDesc(), service.getDeps());
 		services.add(createdService);
 		return createdService;
 	}
